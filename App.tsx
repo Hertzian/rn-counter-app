a54d7e6834e06e65a5a2868f69430cbc3a9d2589
Lalo Aguilar <tallitosan@gmail.com>
2021-04-28 15:02:03 -0500

    alitght items

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 //import HelloWorldScreen from './src/screens/HelloWorldScreen';
-import {CounterScreen} from './src/screens/CounterScreen';
+//import {CounterScreen} from './src/screens/CounterScreen';
+//import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
+//import {DimensionsScreen} from './src/screens/DimensionsScreen';
+//import {PositionScreen} from './src/screens/PositionScreen';
+import {FlexScreen} from './src/screens/FlexScreen';
 
 export const App = () => {
   return (
+    <SafeAreaView style={styles.areaView}>
+      <FlexScreen />
+    </SafeAreaView>
+    //<PositionScreen />
+    //<DimensionsScreen />
+    //<BoxObjectModelScreen />
     //<HelloWorldScreen />
-    <CounterScreen />
+    //<CounterScreen />
   );
 };
+
+const styles = StyleSheet.create({
+  areaView: {
+    flex: 1,
+    //height: 400,
+    //width: 300,
+  },
+});
---
+++ src/screens/FlexScreen.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export const FlexScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //height: 300,
+    backgroundColor: '#28C4D9',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+
+  box1: {
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 30,
+  },
+
+  box2: {
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 30,
+  },
+
+  box3: {
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 30,
+  },
+});
