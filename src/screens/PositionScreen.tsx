9c557cec3a720701f325ed28d9b0144d432441b6
Lalo Aguilar <tallitosan@gmail.com>
2021-04-28 15:03:33 -0500

    position

---
+++ src/screens/PositionScreen.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+export const PositionScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.greenBox} />
+      <View style={styles.violetBox} />
+      <View style={styles.orangeBox} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    //width: 400,
+    //height: 300,
+    flex: 1,
+    //justifyContent: 'center',
+    //alignItems: 'center',
+    backgroundColor: '#28C4D9',
+  },
+
+  //relative position as default
+  violetBox: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#5856D6',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+
+  orangeBox: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#F0A23B',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+
+  greenBox: {
+    //width: 100,
+    //height: 100,
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: 'white',
+    //position: 'absolute',
+    //bottom: 0,
+    //left: 0,
+    //right: 0,
+    //top: 0,
+    ...StyleSheet.absoluteFillObject,
+  },
+});
