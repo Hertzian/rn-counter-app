feb22e60280a6c82718546a01450839582182bd9
Lalo Aguilar <tallitosan@gmail.com>
2021-04-28 15:16:01 -0500

    flex wrap

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -7,6 +7,33 @@ export const FlexScreen = () => {
       <Text style={styles.box1}>Box 1</Text>
       <Text style={styles.box2}>Box 2</Text>
       <Text style={styles.box3}>Box 3</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box1}>Box 1</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
+      <Text style={styles.box2}>Box 2</Text>
+      <Text style={styles.box3}>Box 3</Text>
     </View>
   );
 };
@@ -16,29 +43,27 @@ const styles = StyleSheet.create({
     flex: 1,
     //height: 300,
     backgroundColor: '#28C4D9',
+    justifyContent: 'center',
+    alignItems: 'center',
     flexDirection: 'row',
-    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
   },
 
   box1: {
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
-    //alignSelf has priority over his parent
-    alignSelf: 'center',
   },
 
   box2: {
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
-    alignSelf: 'flex-start',
   },
 
   box3: {
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
-    alignSelf: 'flex-end',
   },
 });
